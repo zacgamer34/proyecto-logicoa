2b30461f7a00f334321be5e2d5d7a5fa56972ce7
zacgamer34 <flashpipes@gmail.com>
2026-05-15 17:39:55 -0500

    sistema de selecion del usuario
    commit 2.1: se integro un sistema de seleccion para el usuario

--- ventaTikets.py
+++ ventaTikets.py
@@ -8,6 +8,9 @@ try:
     archivoMapa=""
     archivoPrecios=""
     datosLogin=""
+    selecionEvento=""
+    eventoselecionado=""
+    datosEvento=""
 #datos de salida
 #variables adicionales
     datosLoginSeparados=""
@@ -18,23 +21,24 @@ try:
     nombreArchivo=""
     idArchivo=""
     validacionUsuario=bool
+    eventos=""
 #proceso
-    while True:
+    
         #el dato entra
-        datosLogin=input("ingrese su nombre y su id[separados por: |]\n: ")
+    datosLogin=input("ingrese su nombre y su id[separados por: |]\n: ")
         #se procesa
-        datosLoginSeparados=datosLogin.replace(" ","").replace("\t","").replace("\n","").split("|")
+    datosLoginSeparados=datosLogin.replace(" ","").replace("\t","").replace("\n","").split("|")
         
-        nombre=datosLoginSeparados[0]
-        id=datosLoginSeparados[1]
+    nombre=datosLoginSeparados[0]
+    id=datosLoginSeparados[1]
         
         
         #abrimos el archivo y lo leemos
-        archivoUsuarios=open("recursos/clientes.txt", "r")
-        archivoUsuarios.readline()
-        validacionUsuario=False
+    archivoUsuarios=open("recursos/clientes.txt", "r")
+    archivoUsuarios.readline()
+    validacionUsuario=False
         #entra al ciclo y se separa
-        for login in archivoUsuarios:
+    for login in archivoUsuarios:
             contenido_clientes=login.replace("\t","").replace(" ","").replace("\n","").split("|")
             
             nombreArchivo=contenido_clientes[0]
@@ -45,13 +49,34 @@ try:
                 validacionUsuario=True
                 break
         
-        if not validacionUsuario:
-            print("usuario o id invalidos")
+    if not validacionUsuario:
+        print("usuario o id invalidos")
     
     
+    selecionEvento=input("desea ver la lista de eventos\nsi \nno").upper()
+    
+    if selecionEvento=="SI":
+        archivoEventos=open("recursos/eventos.txt", "r")
+        
+        eventos=archivoEventos.read()
+    
+    print(eventos)
+    while True:
+        eventoselecionado=input(f"a que evento desea registrarse :{nombre}\n las opciones. pueden ser numeradas de [1-3]en el orden mostrado en pantalla\nseleccion: ")
+        if 1<=eventoselecionado <=3:
+            eventoselecionado=int(eventoselecionado)
+            break
+        else:
+            print("fuera de rango disponible porfavor vuelva a intentarlos")
+            
+        
+        if eventos==1:
+            datosEvento=open()
+            
 
 except Exception as e:
     print(F"error general del sistema->{e}")
 
 finally:
     archivoUsuarios.close
+    archivoEventos.close
